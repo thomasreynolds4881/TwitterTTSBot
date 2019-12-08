e2a8edd749a0b309de0f00bc333ea8c1ddc283a1
Thomas Reynolds <48138681+thomasreynolds4881@users.noreply.github.com>
2019-12-08 08:13:07 -0600

    Can now use txt instead of json

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 '''
-mode 0: fetch from account list (IDs can be found using https://tweeterid.com/)
+mode 0: fetch from account list
 mode 1: fetch with phrase list
 show_retweets: option to show or skip retweets
-log_name: destination file in which to log tweets (json recommended)
+log_name: destination file in which to log tweets
 
 '''
 
@@ -10,4 +10,4 @@ mode = 1
 phrase_list = ["test"]
 account_list = ["Twitter"]
 show_retweets = True
-log_name = "tweetlog.json"
+log_name = "tweetlog.txt"

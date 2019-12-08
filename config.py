d689fb8a96a5c08086782074a66c306647f7311c
Thomas Reynolds <48138681+thomasreynolds4881@users.noreply.github.com>
2019-12-08 07:11:11 -0600

    Updated to take usernames in the account_list

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 '''
-mode 0: fetch from account_list (IDs can be found using https://tweeterid.com/)
-mode 1: fetch with phrase_list
+mode 0: fetch from account list (IDs can be found using https://tweeterid.com/)
+mode 1: fetch with phrase list
 show_retweets: option to show or skip retweets
-log_name: destination file in which to log tweets
+log_name: destination file in which to log tweets (json recommended)
+
 '''
 
 mode = 1
 phrase_list = ["test"]
-account_list = ["783214"]
-show_retweets = False
+account_list = ["Twitter"]
+show_retweets = True
 log_name = "tweetlog.json"

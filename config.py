317b2bd4bcb8656fa797792410c2f993cedf6d5b
Thomas Reynolds <48138681+thomasreynolds4881@users.noreply.github.com>
2019-12-08 05:48:03 -0600

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ log_name: destination file in which to log tweets
 
 mode = 1
 phrase_list = ["test"]
-account_list = ["1169631657185464320"]
+account_list = ["783214"]
 show_retweets = False
-log_name = "tweetlog.txt"
+log_name = "tweetlog.json"
